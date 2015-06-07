197bb4f08e5487abfe0347b4f61f504364c10ca0
Jacob Arthur <jacobaarthur@gmail.com>
2015-06-07 09:50:52 -0500

    Spell checking

--- src/util/simpleDataFormat.h
+++ src/util/simpleDataFormat.h
@@ -1,15 +1,15 @@
 #pragma once
 
-// The Simple Data Formet (.sdf)
+// The Simple Data Format (.sdf)
 // This is a simple text format that can be directly parsed into a Data format
 //
-// The format is a simple key value pair seperated by a colon ':'
+// The format is a simple key value pair separated by a colon ':'
 //      x: 24
-// multiple enteries are sperated by a comma ','
+// multiple entries are separated by a comma ','
 //      x: 24, y: 32
-// list values are wrapped in square brakets '[' and ']' and list enteries are sperated by a comma
+// list values are wrapped in square brackets '[' and ']' and list entries are separated by a comma
 //      point: [24, 32]
-// any white space between surranding the keys and values are trimmed
+// any white space between surrounding the keys and values are trimmed
 
 //TODO: Figure out an error handling method
 
